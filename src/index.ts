bfcc4b32839c02a202bf635dbcd69a9d33af0dc0
Eric Siebeneich <eric.siebeneich@external.iherb.com>
2021-11-01 17:27:05 -0500

    Added exported types

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,7 @@
 // istanbul ignore file
-export { default } from './gwt';
+export {
+  default,
+  GwtDefinition,
+  TestFunction,
+} from './gwt';
 export { default as TestContext } from './contextProvider';
